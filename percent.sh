66750f882eb047d9ebddb57ebbfb9616ac28ed61
Nate <lander@kicksite.net>
2021-02-07 10:38:35 -0600

    Add a dim-zero-padded percent function

---
+++ percent.sh
@@ -0,0 +1,15 @@
+print_percent() {
+  percent=$(echo "$1" | sed 's/^0*\([0-9]*\)\(\..*\)*/\1/g')
+  ndigits=$()
+
+  percent=${percent:-0}
+
+  printf "%03d" $percent | sed 's/^\(0*\)/'"[2m"'\1'"[0m"'/'
+}
+
+for percent in $(seq 0 100); do
+  echo -ne "\r"
+  print_percent $percent
+  echo -ne "% "
+  sleep 0.2
+done
