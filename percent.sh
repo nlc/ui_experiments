d096379018c453fdb313c06654dbeddf50f0df78
Nate <lander@kicksite.net>
2021-02-09 14:32:43 -0600

    add to menu

--- percent.sh
+++ percent.sh
@@ -4,12 +4,14 @@ print_percent() {
 
   percent=${percent:-0}
 
-  printf "%03d" $percent | sed 's/^\(0*\)/'"[2m"'\1'"[0m"'/'
+  printf "%03d" $percent | sed 's/^\(0*\)/'"[2m"'\1'"[22m"'/'
 }
 
+echo -ne "\033[33m"
 for percent in $(seq 0 100); do
   echo -ne "\r"
   print_percent $percent
   echo -ne "% "
   sleep 0.2
 done
+echo -ne "\033[0m"
