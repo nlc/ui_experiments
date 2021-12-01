c7ebe0956f0e2b28b04bf1e0840bdef1595d2e68
nlc <natechav1@gmail.com>
2021-11-30 20:01:07 -0600

    do more stuff with startrek.rb

--- startrek.rb
+++ startrek.rb
@@ -4,7 +4,14 @@ def generate_line
   h = rand(24)
   m = rand(60)
   s = rand(0.0..60.0)
-  keyword = %w[FWS RET XXR DTM PLI X10 EXO GRN APP ELE PWR TSR SRS NFA QRY ADV NUL COM ADV REG].sample
+  suffix =
+    if rand < 0.05
+      %w[a b e r].sample
+    else
+      ' '
+    end
+  number1 = ((rand 20..800) / 8) * 8 - (rand < 0.01 ? 1 : 0)
+  keyword = %w[FWS RET XXR DTM PLI X10 EXO GRN APP ELE PWR TSR SRS NFA QRY ADV NUL COM ADV REG APL].sample
   hash = 32.times.map{'0123456789abcdef'.chars.sample}.join
   sep =
     case keyword
@@ -15,9 +22,9 @@ def generate_line
     else
       '::'
     end
-  status = %w[A A A A A A A A A A B B B C X R T Z P].sample
-  number = rand(110000)
-  '[%02d:%02d:%05.2f] %s/%s %s {%s}%08d' % [h, m, s, keyword, hash, sep, status, number]
+  status = %w[A A A A A A A A A A B B B C X R T Z P ?].sample
+  number2 = rand(110000)
+  '[%02d:%02d:%06.3f]% 4d%s %s/%s %s {%s}%08d' % [h, m, s, number1, suffix, keyword, hash, sep, status, number2]
 end
 
 def refreshing_list
@@ -35,7 +42,7 @@ def refreshing_list
       else
         puts generate_line
       end
-      sleep sleeptime
+      sleep sleeptime * rand(0.5..2.0)
     end
     sleep rand(0.3..0.5)
   end
@@ -49,4 +56,73 @@ def changing_line
   end
 end
 
-refreshing_list
+def moving_focus
+  loop do
+    num_lines = 20
+    lines = num_lines.times.map { generate_line }
+
+    print "\033[2J\033[H"
+    lines.each { |line| puts line }
+
+    hl_line = rand(num_lines)
+    (rand(3..8)).times do
+      print "\033[#{hl_line + 1}H"
+      print lines[hl_line]
+
+      hl_line = rand(num_lines)
+      print "\033[#{hl_line + 1}H"
+      print "\033[1m"
+      print lines[hl_line]
+      print "\033[0m"
+
+      sleep rand(0.1..1.0)
+    end
+  end
+end
+
+def list_and_scroll_hl
+  print "\033[?25l"
+  loop do
+    sleeptime = rand(0.0..0.1)
+
+    print "\033[2J\033[H"
+    sleep rand(0.1..0.3)
+
+    num_lines = rand(5..25)
+    lines = num_lines.times.map { generate_line }
+
+    lines.each do |line|
+      puts line
+
+      sleep sleeptime * rand(0.5..2.0)
+    end
+
+    num_hl_lines = rand(3..6)
+    hl_lines = (0...num_lines).to_a.sample(num_hl_lines).sort
+
+    last_hl_line = nil
+    hl_lines.each do |hl_line|
+      if last_hl_line
+        print "\033[#{last_hl_line + 1}H"
+        print "\033[0m"
+        print lines[last_hl_line]
+      end
+
+      print "\033[#{hl_line + 1}H"
+      print "\033[1m"
+      print lines[hl_line]
+      print "\033[0m"
+
+      last_hl_line = hl_line
+
+      sleep rand(0.1..0.5)
+    end
+
+    sleep 0.5
+  end
+rescue Interrupt
+  print "\033[2J\033[H"
+  print "\033[?25h"
+end
+
+list_and_scroll_hl
