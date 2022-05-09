fa4f8ac1789fef2ebd4c06492f8fc3d897f66944
nlc <natechav1@gmail.com>
2022-05-09 16:57:21 -0500

    update random_blocks to be more random and have braille

--- random_blocks.sh
+++ random_blocks.sh
@@ -1,9 +1,27 @@
-for i in $(seq 10000); do
-  if ((RANDOM % 2)); then
-    echo -ne "▄"
-  else
-    echo -ne "▀"
-  fi
-done
+random_blocks() {
+  n=${1:-1024}
+  for i in $(seq $n); do
+    case $((RANDOM % 4)) in
+      0)
+        echo -ne " "
+      ;;
+      1)
+        echo -ne "▄"
+      ;;
+      2)
+      echo -ne "▀"
+      ;;
+      3)
+        echo -ne "█"
+      ;;
+    esac
+  done
+}
 
-# █
+# for i in $(seq 10240 10495); do echo -ne "$(printf '\\u%04x' $i)"; done
+random_braille() {
+  n=${1:-1024}
+  for i in $(seq $n); do
+    echo -ne "$(printf '\\u%04x' $((10240 + (RANDOM % 256))))"
+  done
+}
